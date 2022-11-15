d3517c9f2bfb0991ca5e29a96124cbf53653aa9e
ArturYalik <lyo3a6rata@gmail.com>
2022-11-15 20:24:29 +0200

    Set

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {System.out.println("Hello world!");
+        int a = 10;
+        int b = 2;
+    calculate(a,b);
     }
+
+    private static void calculate(int a, int b) {
+        int c = a + b;
+       System.out.println(c);
+    }
+
+
 }
