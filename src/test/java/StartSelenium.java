cbe104127e83dbc3d37169fef75d46e13cb1f716
ArturYalik <lyo3a6rata@gmail.com>
2022-11-18 11:54:55 +0200

    ArturYalik

--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -1,8 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class StartSelenium {
     WebDriver wd;
@@ -15,10 +19,16 @@ public class StartSelenium {
     @Test
     public void testLogin(){
         System.out.println("Test Started");
+        WebElement element = wd.findElement(By.tagName("a"));
+        List<WebElement> elements = wd.findElements(By.tagName("div"));
+        System.out.println(elements.size());
+        List<WebElement> home = wd.findElements(By.linkText("HOME"));
+        System.out.println(home.size());
     }
 
     @Test
     public void testName() {
+
         System.out.println("Test Completed");
     }
 
